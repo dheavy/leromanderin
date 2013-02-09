f406d72952784c98dbbb5bb0c2aff28b3b7f9a2b
Davy Peter Braun <davypeterbraun@gmail.com>
2013-02-09 13:53:41 +0100

    Added title tags to navigation.

--- wp-content/themes/leromanderin/navigation.php
+++ wp-content/themes/leromanderin/navigation.php
@@ -26,15 +26,15 @@ if ($pages) {
       </button>
       <div class="nav-collapse collapse">
         <ul class="nav">
-          <li <?php if ($current == '/') echo 'class="active"' ?>><a href="<?php echo site_url() ?>" alt="Retourner &grave; l'accueil du site">Accueil</a></li>
+          <li <?php if ($current == '/') echo 'class="active"' ?>><a href="<?php echo site_url() ?>" title="Retourner &grave; l'accueil du site" alt="Retourner &grave; l'accueil du site">Accueil</a></li>
           <li class="dropdown <?php if ($current == 'chapitre') echo ' active' ?>">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown" alt="Acc&eacute;der aux chapitres">Chapitres <b class="caret"></b></a>
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" title="Acc&eacute;der aux chapitres" alt="Acc&eacute;der aux chapitres">Chapitres <b class="caret"></b></a>
             <ul class="dropdown-menu">
               <?php
               if (count($part1) > 0) {
                 echo '<li class="nav-header">Partie 1</li>' . "\n";
                 foreach($part1 as $chap) {
-                  echo '<li><a href="' . get_permalink($chap->ID) . '"  alt="Cliquez ici pour lire le ' . $chap->post_title . ' du roman">' . $chap->post_title . '</a></li>' . "\n";
+                  echo '<li><a href="' . get_permalink($chap->ID) . '" title="Cliquez ici pour lire le ' . $chap->post_title . ' du roman" alt="Cliquez ici pour lire le ' . $chap->post_title . ' du roman">' . $chap->post_title . '</a></li>' . "\n";
                 }
               }
               ?>
@@ -43,18 +43,18 @@ if ($pages) {
                 echo '<li class="divider"></li>' . "\n";
                 echo '<li class="nav-header">Partie 2</li>' . "\n";
                 foreach($part2 as $chap) {
-                  echo '<li><a href="' . get_permalink($chap->ID) . '"  alt="Cliquez ici pour lire le ' . $chap->post_title . ' du roman">' . $chap->post_title . '</a></li>' . "\n";
+                  echo '<li><a href="' . get_permalink($chap->ID) . '" title="Cliquez ici pour lire le ' . $chap->post_title . ' du roman" alt="Cliquez ici pour lire le ' . $chap->post_title . ' du roman">' . $chap->post_title . '</a></li>' . "\n";
                 }
               }
               ?>
             </ul>
           </li>
-          <li><a href="#social" class="to-comments" onclick="_gaq.push(['_trackEvent', 'Nav Actions', 'Comments', 'Go To Comments']);" alt="Acc&eacute;der aux commentaires de cette page">Commenter</a></li>
+          <li><a href="#social" class="to-comments" onclick="_gaq.push(['_trackEvent', 'Nav Actions', 'Comments', 'Go To Comments']);" title="Acc&eacute;der aux commentaires de cette page" alt="Acc&eacute;der aux commentaires de cette page">Commenter</a></li>
           <li <?php if ($current == 'newsletter') echo 'class="active"' ?>>
-            <a href="<?php echo site_url('/newsletter/') ?>"  alt="Aller &grave; la page d'abonnement &grave; la liste d'information pour être pr&eacute;venu des nouveaux chapitres" onclick="_gaq.push(['_trackEvent', 'Nav Actions', 'Newsletter', 'Go To Newsletter']);">Recevoir un mail aux prochains chapitres</a>
+            <a href="<?php echo site_url('/newsletter/') ?>" title="Aller &grave; la page d'abonnement &grave; la liste d'information pour être pr&eacute;venu des nouveaux chapitres" alt="Aller &grave; la page d'abonnement &grave; la liste d'information pour être pr&eacute;venu des nouveaux chapitres" onclick="_gaq.push(['_trackEvent', 'Nav Actions', 'Newsletter', 'Go To Newsletter']);">Recevoir un mail aux prochains chapitres</a>
           </li>
           <li>
-            <a href="<?php echo site_url('/contact/') ?>" onclick="_gaq.push(['_trackEvent', 'Nav Actions', 'Contact', 'Go To Contact']);" alt="Aller &grave; la page de contact">Contact</a>
+            <a href="<?php echo site_url('/contact/') ?>" onclick="_gaq.push(['_trackEvent', 'Nav Actions', 'Contact', 'Go To Contact']);" title="Aller &grave; la page de contact" alt="Aller &grave; la page de contact">Contact</a>
           </li>
         </ul>
       </div>
