d947008f09ca772fd8bb19932b3283d12dd075e0
Davy Peter Braun <davypeterbraun@gmail.com>
2013-02-13 20:34:03 +0100

    Tweaks on CSS. Removed Pin It button.

--- wp-content/themes/leromanderin/social.php
+++ wp-content/themes/leromanderin/social.php
@@ -10,9 +10,6 @@
       <li class="gplus-btn">
         <g:plus action="share" lang="fr_FR" onendinteraction="endInteraction"></g:plus>
       </li>
-      <li class="pin-btn">
-        <a onclick="_gaq.push(['_trackEvent', 'Social Actions', 'Pinterest', 'Pin It']);"href="http://pinterest.com/pin/create/button/?url=<?php the_permalink(); ?>&media=<?php if(function_exists('the_post_thumbnail')) echo wp_get_attachment_url(get_post_thumbnail_id()); ?>&description=<?php echo rawurlencode("Lisez en ligne 'Erin', roman érotique et fascinant, en cours d'écriture par un auteur anonyme.") ?>" class="pin-it-button" count-layout="horizontal"><img src="//assets.pinterest.com/images/PinExt.png" alt="Pin it" / ></a> <script type="text/javascript" src="http://assets.pinterest.com/js/pinit.js"></script>
-      </li>
       <li class="fb-btn">
         <div class="fb-like" data-href="http://leromanderin.com/" data-send="true" data-layout="button_count" data-width="450" data-show-faces="false" data-font="trebuchet ms"></div>
       </li>
