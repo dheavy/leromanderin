f504228c7c18b02cfdf9dc52e381580969cb3b2f
Davy Peter Braun <davypeterbraun@gmail.com>
2013-02-17 14:10:36 +0100

    Corrected Twitter button code to add URL.

--- wp-content/themes/leromanderin/social.php
+++ wp-content/themes/leromanderin/social.php
@@ -5,7 +5,7 @@
         <iframe allowtransparency="true" frameborder="0" scrolling="no" src="//platform.twitter.com/widgets/follow_button.html?screen_name=LeRomanDErin&lang=fr" style="width:150px; height:20px;"></iframe>
       </li>
       <li class="tweet-btn">
-        <a onclick="_gaq.push(['_trackEvent', 'Social Actions', 'Tweet', 'Use Tweet Button']);" href="https://twitter.com/intent/tweet?button_hashtag=LeRomanDErin&text=<?php echo rawurlencode("Lisez en ligne 'Erin', roman érotique et fascinant, en cours d'écriture par un auteur anonyme.") ?>" class="twitter-hashtag-button" data-url="TODO: Add bit.ly" data-counturl="TODO: Add site URL with tracking" data-lang="en" data-related="litterature" data-count="horizontal">Tweet</a>  
+        <a onclick="_gaq.push(['_trackEvent', 'Social Actions', 'Tweet', 'Use Tweet Button']);" href="https://twitter.com/intent/tweet?button_hashtag=LeRomanDErin&text=<?php echo rawurlencode("Lisez en ligne 'Erin', roman érotique et fascinant, en cours d'écriture par un auteur anonyme.") ?>" class="twitter-hashtag-button" data-url="http://bit.ly/ErinTw" data-counturl="http://bit.ly/ErinTw" data-lang="fr" data-related="litterature" data-count="horizontal">Tweet</a>  
       </li>
       <li class="gplus-btn">
         <g:plus action="share" lang="fr_FR" onendinteraction="endInteraction"></g:plus>
